91ade0c1b08cdbbcefb0b3d3b06c458d64e54154
John <zaren678@gmail.com>
2013-11-25 22:50:28 -0700

    basic support for new lineup xml format

--- src/com/zaren/HdhomerunSignalMeterLib/data/ChannelScanProgram.java
+++ src/com/zaren/HdhomerunSignalMeterLib/data/ChannelScanProgram.java
@@ -1,159 +1,179 @@
 package com.zaren.HdhomerunSignalMeterLib.data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChannelScanProgram implements Serializable
 {
-   private static final long serialVersionUID = 3131125398380181596L;
+    private static final long serialVersionUID = 3131125398380181596L;
 
-   public static final String PROGRAM_ENCRYPTED = "encrypted";
-   public static final String PROGRAM_CONTROL = "control";
-   public static final String PROGRAM_NODATA = "no data";
-   public static final String PROGRAM_NORMAL = "normal";
-   public static final String PROGRAM_VCHANNEL = "vchannel";  
-   
-   public String programString = "";
-   public int programNumber = 0;
-   public int virtualMajor = 0;
-   public int virtualMinor = 0;
-   public String type = PROGRAM_NORMAL;
-   public String name = "";
-   public boolean mIsSubscribed = true;
-   
-   public ChannelScanProgram()
-   {
-      
-   }
-   
-   public ChannelScanProgram(String aProgramString, 
-         int aProgramNumber,
-         int aVirtualMajor, 
-         int aVirtualMinor,
-         String aType,
-         String aName)
-   {      
-      programString = aProgramString;
-      programNumber = aProgramNumber;
-      virtualMajor = aVirtualMajor;
-      virtualMinor = aVirtualMinor;
-      type = aType;
-      name = aName;
-   }
-   
-   public ChannelScanProgram( String aProgramString, 
-         int aProgramNumber,
-         int aVirtualMajor, 
-         int aVirtualMinor,
-         String aType,
-         String aName,
-         boolean aIsSubscribed )
-   {      
-      programString = aProgramString;
-      programNumber = aProgramNumber;
-      virtualMajor = aVirtualMajor;
-      virtualMinor = aVirtualMinor;
-      type = aType;
-      name = aName;
-      mIsSubscribed = aIsSubscribed;
-   }
+    public static final String PROGRAM_ENCRYPTED = "encrypted";
+    public static final String PROGRAM_CONTROL = "control";
+    public static final String PROGRAM_NODATA = "no data";
+    public static final String PROGRAM_NORMAL = "normal";
+    public static final String PROGRAM_VCHANNEL = "vchannel";
 
-   public void setAllFields(String programString,
-                            int programNumber,
-                            int virtualMajor,
-                            int virtualMinor,
-                            String type,
-                            String name)
-   {
-      this.programString = programString;
-      this.programNumber = programNumber;
-      this.virtualMajor = virtualMajor;
-      this.virtualMinor = virtualMinor;
-      this.type = type;
-      this.name = name;
-   }
+    //important tags
+    public static final String TAG_DRM = "drm";
+    public static final String TAG_FAVORITE = "favorite";
 
-   @Override
-   public int hashCode()
-   {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ( ( name == null ) ? 0 : name.hashCode() );
-      result = prime * result + programNumber;
-      result = prime * result + ( ( type == null ) ? 0 : type.hashCode() );
-      result = prime * result + virtualMajor;
-      result = prime * result + virtualMinor;
-      return result;
-   }
+    public String programString = "";
+    public int programNumber = 0;
+    public int virtualMajor = 0;
+    public int virtualMinor = 0;
+    public String type = PROGRAM_NORMAL;
+    public String name = "";
+    public boolean mIsSubscribed = true;
+    public List<String> tags = new ArrayList<String>();
 
-   @Override
-   public boolean equals( Object obj )
-   {
-      if( this == obj ) return true;
-      if( obj == null ) return false;
-      if( getClass() != obj.getClass() ) return false;
-      ChannelScanProgram other = (ChannelScanProgram) obj;
-      if( name == null )
-      {
-         if( other.name != null ) return false;
-      }
-      else if( !name.equals( other.name ) ) return false;
-      if( programNumber != other.programNumber ) return false;
-      if( type == null )
-      {
-         if( other.type != null ) return false;
-      }
-      else if( !type.equals( other.type ) ) return false;
-      if( virtualMajor != other.virtualMajor ) return false;
-      if( virtualMinor != other.virtualMinor ) return false;
-      return true;
-   }
+    public ChannelScanProgram()
+    {
 
-   /* (non-Javadoc)
-    * @see java.lang.Object#toString()
-    */
-   @Override
-   public String toString()
-   {
-      StringBuilder theBuilder = new StringBuilder();
-      
-      theBuilder.append( programNumber + ": " + virtualMajor );
-      
-      if( virtualMinor != 0 )
-      {
-         theBuilder.append( "." + virtualMinor );
-      }
-      
-      theBuilder.append( " " + name );
-      
-      if( type.equals( PROGRAM_ENCRYPTED ) )
-      {
-         theBuilder.append( " (" + PROGRAM_ENCRYPTED + ")" );
-      }
-      else if( type.equals( PROGRAM_NODATA ) )
-      {
-         theBuilder.append( " (" + PROGRAM_NODATA + ")" );
-      }
-      else if( type.equals( PROGRAM_CONTROL ) )
-      {
-         theBuilder.append( " (" + PROGRAM_CONTROL + ")" );
-      }
-      
-      return theBuilder.toString();
-   }
+    }
 
-   public void setVirtualChannelStatus( TunerVStatus aVirtualChannelStatus )
-   {
-      if( aVirtualChannelStatus != null &&
-          aVirtualChannelStatus.returnStatus > 0 )
-      {
-         mIsSubscribed = !aVirtualChannelStatus.notSubscribed;
-         name = aVirtualChannelStatus.name;
-      }
-   }
+    public ChannelScanProgram( String aProgramString, int aProgramNumber, int aVirtualMajor, int aVirtualMinor, String aType, String aName )
+    {
+        programString = aProgramString;
+        programNumber = aProgramNumber;
+        virtualMajor = aVirtualMajor;
+        virtualMinor = aVirtualMinor;
+        type = aType;
+        name = aName;
+    }
 
-   public boolean isSubscribed()
-   {
-      return mIsSubscribed;
-   }
-   
+    public ChannelScanProgram( String aProgramString, int aProgramNumber, int aVirtualMajor, int aVirtualMinor, String aType, String aName, boolean aIsSubscribed )
+    {
+        programString = aProgramString;
+        programNumber = aProgramNumber;
+        virtualMajor = aVirtualMajor;
+        virtualMinor = aVirtualMinor;
+        type = aType;
+        name = aName;
+        mIsSubscribed = aIsSubscribed;
+    }
+
+    public void setAllFields( String programString, int programNumber, int virtualMajor, int virtualMinor, String type, String name )
+    {
+        this.programString = programString;
+        this.programNumber = programNumber;
+        this.virtualMajor = virtualMajor;
+        this.virtualMinor = virtualMinor;
+        this.type = type;
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( name == null ) ? 0 : name.hashCode() );
+        result = prime * result + programNumber;
+        result = prime * result + ( ( type == null ) ? 0 : type.hashCode() );
+        result = prime * result + virtualMajor;
+        result = prime * result + virtualMinor;
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj )
+    {
+        if( this == obj )
+        {
+            return true;
+        }
+        if( obj == null )
+        {
+            return false;
+        }
+        if( getClass() != obj.getClass() )
+        {
+            return false;
+        }
+        ChannelScanProgram other = (ChannelScanProgram)obj;
+        if( name == null )
+        {
+            if( other.name != null )
+            {
+                return false;
+            }
+        }
+        else if( !name.equals( other.name ) )
+        {
+            return false;
+        }
+        if( programNumber != other.programNumber )
+        {
+            return false;
+        }
+        if( type == null )
+        {
+            if( other.type != null )
+            {
+                return false;
+            }
+        }
+        else if( !type.equals( other.type ) )
+        {
+            return false;
+        }
+        if( virtualMajor != other.virtualMajor )
+        {
+            return false;
+        }
+        if( virtualMinor != other.virtualMinor )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        StringBuilder theBuilder = new StringBuilder();
+
+        theBuilder.append( programNumber + ": " + virtualMajor );
+
+        if( virtualMinor != 0 )
+        {
+            theBuilder.append( "." + virtualMinor );
+        }
+
+        theBuilder.append( " " + name );
+
+        if( type.equals( PROGRAM_ENCRYPTED ) )
+        {
+            theBuilder.append( " (" + PROGRAM_ENCRYPTED + ")" );
+        }
+        else if( type.equals( PROGRAM_NODATA ) )
+        {
+            theBuilder.append( " (" + PROGRAM_NODATA + ")" );
+        }
+        else if( type.equals( PROGRAM_CONTROL ) )
+        {
+            theBuilder.append( " (" + PROGRAM_CONTROL + ")" );
+        }
+
+        return theBuilder.toString();
+    }
+
+    public void setVirtualChannelStatus( TunerVStatus aVirtualChannelStatus )
+    {
+        if( aVirtualChannelStatus != null && aVirtualChannelStatus.returnStatus > 0 )
+        {
+            mIsSubscribed = !aVirtualChannelStatus.notSubscribed;
+            name = aVirtualChannelStatus.name;
+        }
+    }
+
+    public boolean isSubscribed()
+    {
+        return mIsSubscribed;
+    }
+
 }
--- src/com/zaren/HdhomerunSignalMeterLib/data/LineupXMLParser.java
+++ src/com/zaren/HdhomerunSignalMeterLib/data/LineupXMLParser.java
@@ -7,175 +7,193 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LineupXMLParser
 {
-   // We don't use namespaces
-   private static final String ns = null;
-   
-   //tags we're interested in
-   private static final String LINEUP = "Lineup";
-   private static final String PROGRAM = "Program";
-   private static final String GUIDE_NAME = "GuideName";
-   private static final String GUIDE_NUMBER = "GuideNumber";
+    // We don't use namespaces
+    private static final String ns = null;
 
-   
+    //tags we're interested in
+    private static final String LINEUP = "Lineup";
+    private static final String PROGRAM = "Program";
+    private static final String GUIDE_NAME = "GuideName";
+    private static final String GUIDE_NUMBER = "GuideNumber";
+    private static final String TAGS = "Tags";
 
-   private int mProgramNum = 0;
+    private int mProgramNum = 0;
 
-   public ProgramsList parse( InputStream aLineupInput ) throws XmlPullParserException, IOException
-   {
-      try
-      {
-         XmlPullParser theParser = Xml.newPullParser();
-         theParser.setFeature( XmlPullParser.FEATURE_PROCESS_NAMESPACES, false );
-         theParser.setInput( aLineupInput, null );
-         theParser.nextTag();
-         return readFeed( theParser );
-      }
-      finally
-      {
-         aLineupInput.close();
-         mProgramNum = 0;
-      }
-   }
+    public ProgramsList parse( InputStream aLineupInput ) throws XmlPullParserException, IOException
+    {
+        try
+        {
+            XmlPullParser theParser = Xml.newPullParser();
+            theParser.setFeature( XmlPullParser.FEATURE_PROCESS_NAMESPACES, false );
+            theParser.setInput( aLineupInput, null );
+            theParser.nextTag();
+            return readFeed( theParser );
+        }
+        finally
+        {
+            aLineupInput.close();
+            mProgramNum = 0;
+        }
+    }
 
-   private ProgramsList readFeed( XmlPullParser aParser ) throws XmlPullParserException, IOException
-   {
-      ProgramsList thePrograms = new ProgramsList();
+    private ProgramsList readFeed( XmlPullParser aParser ) throws XmlPullParserException, IOException
+    {
+        ProgramsList thePrograms = new ProgramsList();
 
-      aParser.require( XmlPullParser.START_TAG, ns, LINEUP );
-      while( aParser.next() != XmlPullParser.END_TAG )
-      {
-         if( aParser.getEventType() != XmlPullParser.START_TAG )
-         {
-            continue;
-         }
-         String name = aParser.getName();
-         // Starts by looking for the program tag
-         if( name.equals( "Program" ) )
-         {
-            try
+        aParser.require( XmlPullParser.START_TAG, ns, LINEUP );
+        while( aParser.next() != XmlPullParser.END_TAG )
+        {
+            if( aParser.getEventType() != XmlPullParser.START_TAG )
             {
-               ChannelScanProgram theProgram = readProgram( aParser );
-               thePrograms.append( theProgram.programNumber, theProgram );
+                continue;
             }
-            catch ( NumberFormatException e )
+            String name = aParser.getName();
+            // Starts by looking for the program tag
+            if( name.equals( "Program" ) )
             {
-               HDHomerunLogger.e( "Failed to parse program from xml: " + e );               
+                try
+                {
+                    ChannelScanProgram theProgram = readProgram( aParser );
+                    thePrograms.append( theProgram.programNumber, theProgram );
+                }
+                catch( NumberFormatException e )
+                {
+                    HDHomerunLogger.e( "Failed to parse program from xml: " + e );
+                }
             }
-         }
-         else
-         {
-            skip( aParser );
-         }
-      }
-      return thePrograms;
-   }
+            else
+            {
+                skip( aParser );
+            }
+        }
+        return thePrograms;
+    }
 
-   private ChannelScanProgram readProgram( XmlPullParser aParser ) throws XmlPullParserException, IOException
-   {
-      aParser.require( XmlPullParser.START_TAG, ns, PROGRAM );
+    private ChannelScanProgram readProgram( XmlPullParser aParser ) throws XmlPullParserException, IOException
+    {
+        aParser.require( XmlPullParser.START_TAG, ns, PROGRAM );
 
-      ChannelScanProgram theProgram = new ChannelScanProgram();
-      
-      while( aParser.next() != XmlPullParser.END_TAG )
-      {
-         if( aParser.getEventType() != XmlPullParser.START_TAG )
-         {
-            continue;
-         }
+        ChannelScanProgram theProgram = new ChannelScanProgram();
 
-         String name = aParser.getName();
-         if( name.equals( GUIDE_NUMBER ) )
-         {
-            String theGuideNumberStr = readGuideNumber( aParser );
-            parseGuideNumber( theGuideNumberStr, theProgram );
-         }
-         else if( name.equals( GUIDE_NAME ) )
-         {
-            String theName = readGuideName( aParser );
-            theProgram.name = theName;
-            theProgram.programString = theName;
-         }
-         else
-         {
-            skip( aParser );
-         }
-      }
-      mProgramNum++;
-      
-      theProgram.programNumber = mProgramNum;
-      theProgram.type = ChannelScanProgram.PROGRAM_VCHANNEL;
-          
-      return theProgram;      
-   }
+        while( aParser.next() != XmlPullParser.END_TAG )
+        {
+            if( aParser.getEventType() != XmlPullParser.START_TAG )
+            {
+                continue;
+            }
 
-   private void parseGuideNumber( String aGuideNumberStr, ChannelScanProgram aProgram )
-   {
-      int thePointIndex = aGuideNumberStr.indexOf( '.' );
-      
-      if( thePointIndex > -1 )
-      {
-         String theMajorNum = aGuideNumberStr.substring( 0, thePointIndex );
-         String theMinorNum = aGuideNumberStr.substring( thePointIndex + 1, aGuideNumberStr.length() );
-         
-         aProgram.virtualMajor = Integer.parseInt( theMajorNum );
-         aProgram.virtualMinor = Integer.parseInt( theMinorNum );
-      }
-      else
-      {
-         aProgram.virtualMajor = Integer.parseInt( aGuideNumberStr );
-         aProgram.virtualMinor = 0;
-      }
-   }
+            String name = aParser.getName();
+            if( name.equals( GUIDE_NUMBER ) )
+            {
+                String theGuideNumberStr = readGuideNumber( aParser );
+                parseGuideNumber( theGuideNumberStr, theProgram );
+            }
+            else if( name.equals( GUIDE_NAME ) )
+            {
+                String theName = readGuideName( aParser );
+                theProgram.name = theName;
+                theProgram.programString = theName;
+            }
+            else if( name.equals( TAGS ) )
+            {
+                List<String> theTags = readTags( aParser );
+                theProgram.tags = theTags;
+            }
+            else
+            {
+                skip( aParser );
+            }
+        }
+        mProgramNum++;
 
-   private String readGuideNumber( XmlPullParser aParser ) throws IOException, XmlPullParserException
-   {
+        theProgram.programNumber = mProgramNum;
+        theProgram.type = ChannelScanProgram.PROGRAM_VCHANNEL;
+
+        return theProgram;
+    }
+
+    private void parseGuideNumber( String aGuideNumberStr, ChannelScanProgram aProgram )
+    {
+        int thePointIndex = aGuideNumberStr.indexOf( '.' );
+
+        if( thePointIndex > -1 )
+        {
+            String theMajorNum = aGuideNumberStr.substring( 0, thePointIndex );
+            String theMinorNum = aGuideNumberStr.substring( thePointIndex + 1, aGuideNumberStr.length() );
+
+            aProgram.virtualMajor = Integer.parseInt( theMajorNum );
+            aProgram.virtualMinor = Integer.parseInt( theMinorNum );
+        }
+        else
+        {
+            aProgram.virtualMajor = Integer.parseInt( aGuideNumberStr );
+            aProgram.virtualMinor = 0;
+        }
+    }
+
+    private String readGuideNumber( XmlPullParser aParser ) throws IOException, XmlPullParserException
+    {
         aParser.require( XmlPullParser.START_TAG, ns, GUIDE_NUMBER );
         String theGuideNumber = readText( aParser );
         aParser.require( XmlPullParser.END_TAG, ns, GUIDE_NUMBER );
         return theGuideNumber;
-   }
-   
-   private String readGuideName( XmlPullParser aParser ) throws IOException, XmlPullParserException
-   {     
-      aParser.require( XmlPullParser.START_TAG, ns, GUIDE_NAME );
-      String theGuideName = readText( aParser );
-      aParser.require( XmlPullParser.END_TAG, ns, GUIDE_NAME );         
-      
-      return theGuideName;
-   }
+    }
 
-   private String readText( XmlPullParser aParser ) throws IOException, XmlPullParserException
-   {
-      String theResult = "";
-      if( aParser.next() == XmlPullParser.TEXT )
-      {
-         theResult = aParser.getText();
-         aParser.nextTag();
-      }
-      return theResult;
-   }
+    private String readGuideName( XmlPullParser aParser ) throws IOException, XmlPullParserException
+    {
+        aParser.require( XmlPullParser.START_TAG, ns, GUIDE_NAME );
+        String theGuideName = readText( aParser );
+        aParser.require( XmlPullParser.END_TAG, ns, GUIDE_NAME );
 
-   private void skip( XmlPullParser aParser ) throws XmlPullParserException, IOException
-   {
-      if( aParser.getEventType() != XmlPullParser.START_TAG )
-      {
-         throw new IllegalStateException();
-      }
-      int theDepth = 1;
-      while( theDepth != 0 )
-      {
-         switch( aParser.next() )
-         {
-            case XmlPullParser.END_TAG:
-               theDepth--;
-               break;
-            case XmlPullParser.START_TAG:
-               theDepth++;
-               break;
-         }
-      }
-   }
+        return theGuideName;
+    }
+
+    private List<String> readTags( final XmlPullParser aParser ) throws IOException, XmlPullParserException
+    {
+        aParser.require( XmlPullParser.START_TAG, ns, TAGS );
+        String theTags = readText( aParser );
+        String[] theTagList = theTags.split( "," );
+        ArrayList<String> theReturnList = new ArrayList<String>( Arrays.asList( theTagList ) );
+        aParser.require( XmlPullParser.END_TAG, ns, TAGS );
+
+        return theReturnList;
+    }
+
+    private String readText( XmlPullParser aParser ) throws IOException, XmlPullParserException
+    {
+        String theResult = "";
+        if( aParser.next() == XmlPullParser.TEXT )
+        {
+            theResult = aParser.getText();
+            aParser.nextTag();
+        }
+        return theResult;
+    }
+
+    private void skip( XmlPullParser aParser ) throws XmlPullParserException, IOException
+    {
+        if( aParser.getEventType() != XmlPullParser.START_TAG )
+        {
+            throw new IllegalStateException();
+        }
+        int theDepth = 1;
+        while( theDepth != 0 )
+        {
+            switch( aParser.next() )
+            {
+                case XmlPullParser.END_TAG:
+                    theDepth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    theDepth++;
+                    break;
+            }
+        }
+    }
 }
